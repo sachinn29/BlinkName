f6d737e8da7fca5554e96aebca8f8556ebcf9232
sachinn29 <112768099+sachinn29@users.noreply.github.com>
2023-08-24 15:45:25 +0530

    Update BlinkName.ino

--- BlinkName/BlinkName.ino
+++ BlinkName/BlinkName.ino
@@ -32,12 +32,144 @@ void dash() {
 }
 
 // Function to convert a character to Morse code
-void morse_encryption(char character) {
-  switch (character) {
-    // Cases for each character's Morse code
-    // ...
+void morse_encryption(char name) {
+  switch (name) {
+    case 'a':
+      dot();
+      dash();
+      break;
+    case 'b':
+      dash();
+      dot();
+      dot();
+      dot();
+      break;
+    case 'c':
+      dash();
+      dot();
+      dash();
+      dot();
+      break;
+    case 'd':
+      dash();
+      dot();
+      dot();
+      break;
+    case 'e':
+      dot();
+      break;
+    case 'f':
+      dot();
+      dot();
+      dash();
+      dot();
+      break;
+    case 'g':
+      dash();
+      dash();
+      dot();
+      break;
+    case 'h':
+      dot();
+      dot();
+      dot();
+      dot();
+      break;
+    case 'i':
+      dot();
+      dot();
+      break;
+    case 'j':
+      dot();
+      dash();
+      dash();
+      dash();
+      break;
+    case 'k':
+      dash();
+      dot();
+      dash();
+      break;
+    case 'l':
+      dot();
+      dash();
+      dot();
+      dot();
+      break;
+    case 'm':
+      dash();
+      dash();
+      break;
+    case 'n':
+      dash();
+      dot();
+      break;
+    case 'o':
+      dash();
+      dash();
+      dash();
+      break;
+    case 'p':
+      dot();
+      dash();
+      dash();
+      dot();
+      break;
+    case 'q':
+      dash();
+      dash();
+      dot();
+      dash();
+      break;
+    case 'r':
+      dot();
+      dash();
+      dot();
+      break;
+    case 's':
+      dot();
+      dot();
+      dot();
+      break;
+    case 't':
+      dash();
+      break;
+    case 'u':
+      dot();
+      dot();
+      dash();
+      break;
+    case 'v':
+      dot();
+      dot();
+      dot();
+      dash();
+      break;
+    case 'w':
+      dot();
+      dash();
+      dash();
+      break;
+    case 'x':
+      dash();
+      dot();
+      dot();
+      dash();
+      break;
+    case 'y':
+      dash();
+      dot();
+      dash();
+      dash();
+      break;
+    case 'z':
+      dash();
+      dash();
+      dot();
+      dot();
+      break;
     default:
-      // Do nothing for characters not in the Morse code table
+      // Ignore unknown characters
       break;
   }
 }
