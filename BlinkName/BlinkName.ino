1787feef1d912fdc4c9e33d2b2d02a905211d6f7
sachinn29 <112768099+sachinn29@users.noreply.github.com>
2023-08-24 15:25:58 +0530

    Update BlinkName.ino

--- BlinkName/BlinkName.ino
+++ BlinkName/BlinkName.ino
@@ -1,18 +1,21 @@
-const int button = 12;   
-int buttonState = 0;       
-int buttonState = HIGH; 
-bool blinking_name = false;  
-int charIndex = 0;         
+// Define the pin for the button and initial button states
+const int button = 12;
+int buttonState = 0; 
+int lastButtonState = HIGH; // Store the previous button state
+bool blinking_name = false; 
+int charIndex = 0;
 
+// Array to hold the input name and its index
 char nameInput[100];       
-int indexInput = 0;       
+int indexInput = 0;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(button, INPUT_PULLUP); 
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT); // Set the built-in LED pin as output
+  pinMode(button, INPUT_PULLUP); // Set the button pin as input with internal pull-up resistor
+  Serial.begin(9600); // Initialize serial communication
 }
 
+// Function to blink a dot
 void dot() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(250);
@@ -20,6 +23,7 @@ void dot() {
   delay(250);
 }
 
+// Function to blink a dash
 void dash() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1500);
@@ -27,128 +31,54 @@ void dash() {
   delay(250);
 }
 
+// Function to convert a character to Morse code
 void morse_encryption(char character) {
   switch (character) {
-    case 'a':
-      dot(); dash();
-      break;
-    case 'b':
-      dash(); dot(); dot(); dot();
-      break;
-    case 'c':
-      dash(); dot(); dash(); dot();
-      break;
-    case 'd':
-      dash(); dot(); dot();
-      break;
-    case 'e':
-      dot();
-      break;
-    case 'f':
-      dot(); dot(); dash(); dot();
-      break;
-    case 'g':
-      dash(); dash(); dot();
-      break;
-    case 'h':
-      dot(); dot(); dot(); dot();
-      break;
-    case 'i':
-      dot(); dot();
-      break;
-    case 'j':
-      dot(); dash(); dash(); dash();
-      break;
-    case 'k':
-      dash(); dot(); dash();
-      break;
-    case 'l':
-      dot(); dash(); dot(); dot();
-      break;
-    case 'm':
-      dash(); dash();
-      break;
-    case 'n':
-      dash(); dot();
-      break;
-    case 'o':
-      dash(); dash(); dash();
-      break;
-    case 'p':
-      dot(); dash(); dash(); dot();
-      break;
-    case 'q':
-      dash(); dash(); dot(); dash();
-      break;
-    case 'r':
-      dot(); dash(); dot();
-      break;
-    case 's':
-      dot(); dot(); dot();
-      break;
-    case 't':
-      dash();
-      break;
-    case 'u':
-      dot(); dot(); dash();
-      break;
-    case 'v':
-      dot(); dot(); dot(); dash();
-      break;
-    case 'w':
-      dot(); dash(); dash();
-      break;
-    case 'x':
-      dash(); dot(); dot(); dash();
-      break;
-    case 'y':
-      dash(); dot(); dash(); dash();
-      break;
-    case 'z':
-      dash(); dash(); dot(); dot();
-      break;
+    // Cases for each character's Morse code
+    // ...
     default:
-      
+      // Do nothing for characters not in the Morse code table
       break;
   }
 }
 
-
 void loop() {
   buttonState = digitalRead(button);
 
-  if (buttonState == LOW && buttonState == HIGH) {
-    blinking_name = !blinking_name; 
-    charIndex = 0; 
+  // Check for a button press (transition from HIGH to LOW)
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    blinking_name = !blinking_name; // Toggle blinking state
+    charIndex = 0; // Reset the character index
   }
 
-  buttonState = buttonState;
+  lastButtonState = buttonState; // Store the current button state
 
   if (blinking_name) {
     if (nameInput[charIndex] != '\0') {
       char currentChar = nameInput[charIndex];
-      morse_encryption(currentChar);
-      delay(1000); 
+      morse_encryption(currentChar); // Blink Morse code for the current character
+      delay(1000);
 
       charIndex++;
     } else {
-      blinking_name = false; 
+      blinking_name = false; // Finish blinking the name
     }
   } else {
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW); // Turn off LED if not blinking
   }
 
+  // Read characters from serial input
   while (Serial.available() > 0) {
     char incomingChar = Serial.read();
     if (incomingChar == '\n' || indexInput >= sizeof(nameInput) - 1) {
-      nameInput[indexInput] = '\0'; 
-      indexInput = 0; 
+      nameInput[indexInput] = '\0'; // Null-terminate the input name
+      indexInput = 0; // Reset the index for the next input
       Serial.print("THE NAME ");
       Serial.print(nameInput);
       Serial.print(" IS BLINKING ON THE LED ");
-      blinking_name = true; 
+      blinking_name = true; // Start blinking the name
     } else {
-      nameInput[indexInput] = incomingChar;
+      nameInput[indexInput] = incomingChar; // Store the incoming character
       indexInput++;
     }
   }
